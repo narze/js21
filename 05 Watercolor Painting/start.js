5c58b5ac4a06bdce0356113a538eecba33259251
Manassarn Manoonchai <manassarn@gmail.com>
2020-05-28 21:56:26 +0700

    :construction: 05: Change opacity by distance

--- 05 Watercolor Painting/start.js
+++ 05 Watercolor Painting/start.js
@@ -7,15 +7,22 @@
 
   let previousPoint = { x: 0, y: 0 }
 
+  function getDistance(previousPoint, currentPoint) {
+    return Math.sqrt((previousPoint.x - currentPoint.x) ** 2 + (previousPoint.y - currentPoint.y) ** 2)
+  }
+
   function onMouseMove({ pageX, pageY }) {
     const currentPoint = { x: pageX, y: pageY }
 
     context.beginPath()
 
+    const distance = getDistance(previousPoint, currentPoint)
+    const opacity = Math.min(0.5, 1 / distance)
+    context.strokeStyle = `rgba(222, 20, 109, ${opacity})`
+
     context.lineCap = "round"
     context.lineJoin = "round"
     context.lineWidth = 40
-    context.strokeStyle = `rgba(222, 20, 109, 0.8)`
 
     context.moveTo(previousPoint.x, previousPoint.y)
     context.lineTo(currentPoint.x, currentPoint.y)
