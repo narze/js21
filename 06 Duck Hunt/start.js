641de64a54926e84f3f81b6e8299faceffe1979d
Manassarn Manoonchai <manassarn@gmail.com>
2020-06-01 11:25:47 +0700

    :construction: 06: Create random ducks

--- 06 Duck Hunt/start.js
+++ 06 Duck Hunt/start.js
@@ -1,4 +1,23 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+  function createDucks() {
+    const ducks = [...Array(5)].map(() => {
+      return {
+        x: random(0, window.innerWidth),
+        y: window.innerHeight,
+        speedX: random(-50, 50),
+        speedY: random(5, 10),
+      }
+    })
 
+    console.log(ducks)
+  }
+
+  function run() {
+    createDucks()
+  }
+
+  run()
 })();
