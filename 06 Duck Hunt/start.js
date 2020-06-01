e9d03177c1a3cb76256d3f3a619be7d14a9c1adf
Manassarn Manoonchai <manassarn@gmail.com>
2020-06-01 11:37:29 +0700

    :sparkles: 06: Move ducks

--- 06 Duck Hunt/start.js
+++ 06 Duck Hunt/start.js
@@ -2,8 +2,9 @@
   function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
+
   function createDucks() {
-    const ducks = [...Array(5)].map(() => {
+    return [...Array(5)].map(() => {
       return {
         x: random(0, window.innerWidth),
         y: window.innerHeight,
@@ -11,12 +12,35 @@
         speedY: random(5, 10),
       }
     })
+  }
 
-    console.log(ducks)
+  function setupDuckElement(duck) {
+    const duckEl = document.createElement("div")
+    duckEl.className = "duck"
+    duckEl.style.left = `${duck.x}px`
+    duckEl.style.top = `${duck.y}px`
+    duckEl.style.backgroundImage = "url(./left-1.png)"
+    document.body.appendChild(duckEl)
+
+    return { duck, duckEl }
+  }
+
+  function moveDuck(duckEl, duck) {
+    const { left, top } = duckEl.getBoundingClientRect()
+    duck.x = left + duck.speedX
+    duck.y = top - duck.speedY
+    duckEl.style.left = `${duck.x}px`
+    duckEl.style.top = `${duck.y}px`
   }
 
   function run() {
-    createDucks()
+    const ducks = createDucks()
+
+    const duckElements = ducks.map(setupDuckElement)
+
+    duckElements.forEach(({ duck, duckEl }) => {
+      setInterval(() => moveDuck(duckEl, duck), 100)
+    })
   }
 
   run()
